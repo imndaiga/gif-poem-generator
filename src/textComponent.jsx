57b283458ed66a2c0a78e68913a68a3451bf19b6
ioreka <lucymhodges@gmail.com>
2019-03-04 20:43:42 +0000

    pseudocode for conditionally rendering text component

--- src/textComponent.jsx
+++ src/textComponent.jsx
@@ -1,7 +1,17 @@
 import React, { Component } from 'react'
 
 
-//conditional rendering of text based on ?url?
-
+//conditional rendering of text based on ?url endpoint?
+// if endpoint === "localhost:3000/welcome" {
+//   do this thing
+// } else if endpoint === "localhost:3000/instructions" {
+//   do this other thing
+// } else if endpoint === "localhost:3000/generate" {
+//   do this other thing
+// } else if endpoint === "localhost:3000/results" {
+//   do this other thing
+// } else {
+//   break clause
+// }
 
 export default TextComponent
